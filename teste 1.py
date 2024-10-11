28f1d93d06295bded5f1ca54d249de57ed48043d
richaardbruno <109255248+richaardbruno@users.noreply.github.com>
2024-10-11 10:46:56 -0300

    Add files via upload

---
+++ teste 1.py
@@ -0,0 +1,7 @@
+nome = input ('Qual é o seu nome?')
+print('Olá,' , nome,'!', 'Prazer em te conhecer!')
+idade = input ('Qual é a sua idade?')
+print('Vc tem' , idade,'.' ,  'Correto?')
+peso = input ('E qual é mesmo seu peso?')
+print('Bom peso')
+
